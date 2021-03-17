491d9ebb66e756d8a4acf215d9bd0fab2a2bb4e6
Flávio <flavio.fiori31@gmail.com>
2021-03-17 16:50:06 -0300

    :art: Adiciona refatoração no App.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SideBarProps {
+    genres: Array<{
+        id: number;
+        name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+        title: string;
+    }>;
+    selectedGenreId: number;
+    onHandleClickButton: () => void;
+}
+
+export function SideBar({ genres, selectedGenreId, onHandleClickButton }: SideBarProps) {
+    return(
+
+        <nav className="sidebar">
+
+            <span>Watch<p>Me</p></span>
+
+            <div className="buttons-container">
+                {
+                    genres.map(genre => (
+
+                        <Button
+                            key={ genre.title }
+                            id={ String(genre.id) }
+                            title={ genre.title }
+                            iconName={ genre.name }
+                            onClick={ () => onHandleClickButton(genre.id) }
+                            selected={ selectedGenreId === genre.id }
+                        />
+                    ))}
+            </div>
+
+        </nav>
+
+    )
 }
